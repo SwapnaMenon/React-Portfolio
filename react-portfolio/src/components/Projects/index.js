8ee59586b7f9334f59bd3a2a91343698100b5b25
Swapna <swapnamenon8@gmail.com>
2022-07-22 10:47:14 -0400

    added project link to project page

--- react-portfolio/src/components/Projects/index.js
+++ react-portfolio/src/components/Projects/index.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import Projects from "../Projects";
 
-const Projects = () => {
+const Projects () => {
     return (
         <div className="container" id="repo">
             <div className="row justify-description-center">
             {/* Project 1 */}
                 <div className="col-xs-12 col-sm-12 col-lg-4 mb-4">
-                            <h3>Project 1</h3>
-                            {/* Import image */}
+                            <h3>Project: Foston Pizza</h3>
                             <div className="screenshot">
-                                <img src={`${process.env.PUBLIC_URL}/assets/images/`} 
+                                <img src={`${process.env.PUBLIC_URL}../public/images/foston-pizza`} 
                                 className="d-flex align-self-center"
-                                    alt="" />
+                                    alt="foston pizza" />
                             </div>
                             <div className="card-body mb-5 pl-0">
                                 <p className="card-text">
-                                    {/* Description */}
-                                    
                                 </p>
                                 <div className="url w-100 position-absolute">
-                                    {/* GitHub link */}
-                                    <a href="" target="_blank" className="btn" rel="noreferrer">
-                                        <i className="fab fa-github"></i>
+                                    <a href="https://github.com/project0204/reimagined-system" target="_blank" className="btn" rel="noreferrer">
+                                        <i className="fab fa-githubrepo"></i>
                                     </a>
-                                    {/* Deployed link */}
-                                    <a href="" target="_blank" className="btn mr-2" rel="noreferrer">
-                                        <i className="fas fa-link"></i>
+                                    <a href="https://reimagined-system.herokuapp.com/" target="_blank" className="btn mr-2" rel="noreferrer">
+                                        <i className="fas fa-deployedlink"></i>
                                     </a>
                                 </div>
                             </div>
                 </div> 
+            </div>
+        </div>
+    )
+}
+
                             
   
   
